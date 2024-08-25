1618fbc58edbceece1484a7cb52da3a26dafffed
Gigerin <codenamedelta228@gmail.com>
2024-08-25 21:01:47 +0300

    Add comments

--- app.py
+++ app.py
@@ -3,24 +3,10 @@ from forms import DataForm
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
-from util import get_datetime_after_n_months, calculate_next_month_rate
+from util import get_datetime_after_n_months, calculate_next_month_rate, calculate_amount
 
 app = Flask(__name__)
 
-
-def calculate_amount(date, amount, periods, rate):
-    result = {}
-    current_date = date
-    current_amount = amount
-    for i in range(1, periods + 1):
-
-        current_amount = calculate_next_month_rate(
-            current_amount, rate
-        )
-        current_date = get_datetime_after_n_months(date, i - 1)
-
-        result[str(current_date)] = round(current_amount, 2)
-    return result
 
 
 @app.route("/api/calculate_deposit", methods=["POST"])
